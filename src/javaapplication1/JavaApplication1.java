7c4534f121d0e7dc7c08a46d981c5a08f1118b87
marco2505 <marco.hauser@hs-furtwangen.de>
2017-03-23 11:45:12 +0100

    Add files via upload

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,16 @@
+package javaapplication1;
+
+/**
+ *
+ * @author marco
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("jjj");
+    }
+    
+}
